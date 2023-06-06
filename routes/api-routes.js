0cca4340c91b1c6e9c98e3d3e1935c5d9caf6c07
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-06-06 19:22:52 -0400

    new changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,20 +4,20 @@ const fs = require ("fs");
 
 //the GET request for api routes
 router.get('/api/notes', async (req,res)=>{
-    const dbJSON =await JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    const dbJson =await JSON.parse(fs.readFileSync("../db/db.json","utf8"));
     res.json(dbJson);
 });
 
 //the POST request for api routes
 router.post('/api/notes', (req,res) => {
-    let dbJson= JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    let dbJson= JSON.parse(fs.readFileSync("../db/db.json","utf8"));
     let newFeedback ={
         title:req.body.title,
         text:req.body.text,
         id: uuidv4()
     };
     dbJson.push(newFeedback);
-    fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
+    fs.writeFileSync("../db/db.json",JSON.stringify(dbJson));
     res.json(dbJson)
 });
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const path = require('path');
 
-//get route to send "index.html" as resposne
-
-router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, '../02-Challenge/Develop/public/index.html'))
-});
-
 //get route to send "note.html" as a response
 
 router.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, "../02-Challenge/Develop/public/notes.html"))
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
+
+//get route to send "index.html" as resposne
+
+router.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
 
 module.exports = router;
